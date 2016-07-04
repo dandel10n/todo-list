7c7eddf630876971375688d55de39186195f44b0
Mariya Dergay <mary.dergay@gmail.com>
2016-07-04 20:15:30 +0300

    Added is_completed mass tagging in admin and tagging as is_completed at front-and page

--- todo/admin.py
+++ todo/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Task
 
+def make_completed(modeladmin, request, queryset):
+    queryset.update(is_completed=True)
+make_completed.short_description = "Mark selected tasks as completed"
 
 class TaskAdmin(admin.ModelAdmin):
     list_filter = ('due_date',)
+    actions = [make_completed]
 
 admin.site.register(Task, TaskAdmin)
